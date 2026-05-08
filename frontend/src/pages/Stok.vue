841352bd828ea9536f7c743cae5ebc78c9123696
Adinda Balqis <adindabalqis245@gmail.com>
2026-05-08 19:42:15 +0700

    web

--- frontend/src/pages/Stok.vue
+++ frontend/src/pages/Stok.vue
@@ -16,7 +16,7 @@ const gambar = ref('')
 // 🔥 FETCH DATA
 const fetchData = async () => {
   try {
-    const res = await fetch("https://myshop42.infinityfreeapp.com/produk.php')
+    const res = await fetch('https://myshop42.infinityfreeapp.com/api.php')
 
     if (!res.ok) {
       throw new Error('Response tidak valid')
